6a54425f9c82186a5102b2c0e1725f6874d7e884
Muhammad Syahrulah <muhammadsyahrullah26@gmail.com>
2026-01-30 13:39:27 +0700

    minigames prep

--- app/components/index/join/JoinNow.vue
+++ app/components/index/join/JoinNow.vue
@@ -1,21 +1,32 @@
 <template>
-    <section class="py-24 text-center px-6 md:px-16">
-        <h2 data-aos="fade-up" class="text-4xl font-bold">
-            Join the
-            <span class="text-[var(--gold-main)]">Dewa United</span> Community
-        </h2>
+  <section class="py-24 text-center px-6 md:px-16">
+    <h2 data-aos="fade-up" class="text-4xl font-bold">
+      Join the
+      <span class="text-[var(--gold-main)]">Dewa United</span> Community
+    </h2>
 
-        <p data-aos="fade-up" data-aos-delay="150" class="mt-4 text-gray-400">
-            Stay updated, get limited drops, and represent the squad with pride.
-        </p>
-        <NuxtLink to="https://dewaunited.com/login" target="_blank" external>
-            <button
-            data-aos="zoom-in"
-            data-aos-delay="300"
-            class="mt-10 px-10 py-4 rounded-2xl bg-[var(--gold-main)] text-black font-bold hover:bg-[var(--gold-dark)] transition"
-            >
-            Join Now
-            </button>
-        </NuxtLink>
-    </section>
+    <p data-aos="fade-up" data-aos-delay="150" class="mt-4 text-gray-400">
+      Stay updated, get limited drops, and represent the squad with pride.
+    </p>
+    <div class="flex justify-center gap-4 mt-10">
+      <NuxtLink to="https://dewaunited.com/login" target="_blank" external>
+        <button
+          data-aos="zoom-in"
+          data-aos-delay="300"
+          class="mt-10 px-10 py-4 rounded-2xl bg-[var(--gold-main)] text-black font-bold hover:bg-[var(--gold-dark)] transition"
+        >
+          Join Now
+        </button>
+      </NuxtLink>
+      <NuxtLink to="/minigames" target="_blank" external>
+        <button
+          data-aos="zoom-in"
+          data-aos-delay="300"
+          class="mt-10 px-10 py-4 btn-outline w-full sm:w-auto text-center"
+        >
+          Mini Games
+        </button>
+      </NuxtLink>
+    </div>
+  </section>
 </template>
---
+++ app/pages/minigames/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+<template>
+  <section>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-6 py-10">
+      <h1 class="text-4xl font-bold">Minigames</h1>
+      <div class="bg-zinc-900 rounded border border-white/30 p-6 rounded-lg">
+        <p class="text-lg">
+          Dapatkan point anda dengan bermain minigames seru dari Dewa United!
+        </p>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
+          <div
+            class="bg-zinc-800 rounded-lg pt-4 mt-3 border border-white/30 space-y-3 hover:border-[var(--gold-main)]"
+          >
+            <div class="relative aspect-square bg-zinc-900 overflow-hidden">
+              <img
+                src="/du-universal.png"
+                class="absolute inset-0 w-full h-full object-cover transition-all duration-700 ease-out group-hover:opacity-0 group-hover:scale-110"
+              />
+            </div>
+          </div>
+          <div
+            class="bg-zinc-800 rounded-lg pt-4 mt-3 border border-white/30 space-y-3 hover:border-[var(--gold-main)]"
+          ></div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
